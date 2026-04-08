158879e4bacfc28018c0a31e0b109e2f9e66516e
kndfposer <vm-2107@mail.ru>
2026-04-08 20:37:12 +0300

    type parser2

--- src/main/java/org/example/app/MissionManager.java
+++ src/main/java/org/example/app/MissionManager.java
@@ -15,22 +15,29 @@ public class MissionManager {
     private final Scanner scanner = new Scanner(System.in);
     private final MissionLoaderRegistry loaderRegistry = DefaultMissionLoaderRegistryFactory.createDefault();
     private final MissionValidator validator = new MissionValidator();
-    private MissionReportStrategy reportStrategy = new SummaryMissionReportStrategy();
+    private final MissionReportStrategy reportStrategy = new SummaryMissionReportStrategy();
 
     public void start() {
         System.out.println("Введите 0 вместо пути, чтобы выйти");
+
         while (true) {
             File file = ConsoleFileChooser.choose(scanner);
             if (file == null) {
                 System.out.println("ПРОГРАММА ЗАВЕРШЕНА");
                 return;
             }
+
             try {
                 Mission mission = loaderRegistry.load(file);
                 validator.validate(mission);
+
+                System.out.println();
                 System.out.println(reportStrategy.generate(mission));
+                System.out.println();
+
             } catch (Exception e) {
                 System.out.println("Ошибка: " + e.getMessage());
+                System.out.println();
             }
         }
     }
